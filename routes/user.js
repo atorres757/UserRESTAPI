297b001c47655225aebbebc24ff6403c67199adb
Allen T <atorres757@gmail.com>
2013-03-16 06:57:14 -0700

    Fixed bugs, now able to save, find, and delete user.

--- routes/user.js
+++ routes/user.js
@@ -3,11 +3,7 @@
  * User Controller
  */
 
-exports.index = function(req, res){
-	var mg = require('mongoose');
-	mg.connect('mongodb://localhost/user');
-	
-	var schema = mg.Schema, oId = schema.ObjectId, userSchema, model, user, url = require('url'), query = url.parse(req.url, true).query;
+var mongoose = require('mongoose'), schema = mongoose.Schema, oId = schema.ObjectId, userSchema, model;
 	userSchema = new schema({
 		email: {type: String, lowercase:true, trim: true, required: true, index:{unique:true}},
 		first_name: String,
@@ -16,42 +12,57 @@ exports.index = function(req, res){
 		date_added: Date,
 		last_mod: Date
 	});
-	model = mg.model('user', userSchema, 'users');
+	model = mongoose.model('user', userSchema, 'users');
+
+exports.index = function(req, res){
+	
+	var user, url = require('url'), query = url.parse(req.url, true).query, querystring = require('querystring');
+	mongoose.connect('mongodb://localhost/user');
+	
+	function handleError (err) {
+		console.log(err);
+		mongoose.disconnect();
+		res.send(JSON.stringify({error:true}));
+	}
 	
   switch (req.method) {
   case 'GET':
-	  user = new model();
-	  
-	  /*
-	  user.set('email', 'atorres757@gmail.com');
-	  user.set('first_name', 'Allen');
-	  user.set('last_name', 'Torres');
-	  user.set('date_added', Date.now());
-	  user.set('last_mod', Date.now());
-	  user.save();
-	  */
-	  console.log(query);
-	  mg.disconnect();
-	  res.send('User Get!');
+	  model.findById(query.id, function (err, user) {
+		  if (err) {
+			  handleError(err);
+		  }
+		  mongoose.disconnect();
+		  res.send(JSON.stringify(user));
+	  });
 	  break;
   case 'PUT':
   case 'POST':
-	  /*
-	  user.set('email', 'atorres757@gmail.com');
-	  user.set('first_name', 'Allen');
-	  user.set('last_name', 'Torres');
-	  user.set('date_added', Date.now());
+	  var data = req.body;
+	  user = (data._id)? model.find({'_id':data._id}) : new model();
+	  user.set('email', data.email);
+	  user.set('first_name', data.first_name);
+	  user.set('last_name', data.last_name);
+	  user.set('date_added', data.date_added || Date.now());
 	  user.set('last_mod', Date.now());
-	  user.save();
-	  */
-	  mg.disconnect();
-	  res.send('User Post!');
+	  user.save(function (err) {
+		  handleError(err);
+	  });
+	  mongoose.disconnect();
+	  res.send(JSON.stringify(user));
 	  break;
   case 'DELETE':
-	  mg.disconnect();
-	  res.send('User Delete!');
+	  model.findById(query.id, function (err, user) {
+		  user.remove(function (err, user) {
+			  if (err) {
+				  handleError(err);
+			  }
+			  mongoose.disconnect();
+			  res.send(JSON.stringify(user));
+		  });
+	  });
+	  break;
+  case 'default':
+	  handleError({});
 	  break;
   }
-  
-  res.send('Hello');
 };
